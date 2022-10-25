8179b29309edcde61a0b87cb26091c4f39c46cd8
agungibr <agungibr@outlook.co.id>
2022-10-25 09:35:06 +0700

    book

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('halo', HeloController::class);
+Route::resource('/siswa', SiswaController::class);
+Route::resource('/books', BookController::class);
+Route::resource('/author', AuthorController::class);
